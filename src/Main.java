40d5452f5f3ab3cb8a48a46cca9b35f77a00a6c8
Hema-Hutech <hema@hutechsolutions.com>
2025-11-29 10:45:22 +0530

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Jenkins + Docker + Terraform EC2!");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {}
+        }
+    }
+}
