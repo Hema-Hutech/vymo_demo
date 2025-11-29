8eb70b13706a5f3199f6eaa6e573a77150052b45
Hema-Hutech <hema@hutechsolutions.com>
2025-11-29 10:52:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,56 +6,44 @@ terraform {
     }
   }
 }
-
 provider "aws" {
   region = var.region
 }
-
-resource "aws_key_pair" "jenkins_key" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
+# Security Group
 resource "aws_security_group" "hello_sg" {
   name        = "hello-jenkins-sg"
-  description = "Allow SSH and app port"
-
+  description = "Allow SSH and app access"
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
+    description = "App Port"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "hello-jenkins-sg"
+  }
 }
-
+# EC2 Instance
 resource "aws_instance" "hello_ec2" {
   ami                    = var.ami_id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.jenkins_key.key_name
+  instance_type          = var.instance_type
+  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.hello_sg.id]
-
   tags = {
     Name = "hello-jenkins-ec2"
   }
 }
-
-
-
-
-
-
-
